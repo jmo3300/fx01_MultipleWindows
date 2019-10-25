0b4263f0a4d1535532775d6347caf4ab551b2a43
jmo <jmo@w1001.fritz.box>
2019-10-25 09:38:25 +0200

    Additional Windows & Features for choosing the current window

--- fx01_MultipleWindows/src/main/Main.java
+++ fx01_MultipleWindows/src/main/Main.java
@@ -1,10 +1,15 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -37,15 +42,41 @@ import javafx.stage.Stage;
  * 
  * The present class Main is used as controller for both panes.
  * 
- * 3.2. Additional Windows and feature for selecting the current window
+ * 3.2. Additional Windows
  * 
- * TODO
+ * Copy window0.fxml to window1.fxml and window2.fxml and 
+ * change the text of the Labels in that files to "Window 1" and "Window 2".
+ * 
+ * Create a List<GridPane> windows, clone the initialization of Window 0 for 1 and 2 and add
+ * the created GridPanes to the  windows list.
+ * 
+ * From this list we can chose a window and set it to the current window.
+ * 
+ * 3.3. Features for choosing the current window
+ * 
+ * For choosing the current window we create three buttons. 
+ * Each button makes a particular window the current window.
+ * 
+ * The buttons are collected in a toolbar. The toolbar becomes the first child of the base pane.
+ * Therefore we change the type of the base pane from AnchorPane to VBox in Main class and main.fxml and
+ * add toolbar and buttons to the main.fxml.
+ * 
+ * onAction code of the buttons will be "setWindow0", "setWindow1" and "setWindow2".
+ * 
+ * We inject and implement these messages into the Main class.
+ * 
  *
  * @author jmo3300
  * @version 0.0.1
  */
 
 public class Main extends Application {
+	
+	VBox root;
+	
+	private List<GridPane> windows = new ArrayList<GridPane>();
+	
+	private GridPane windowCurrent;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -57,13 +88,22 @@ public class Main extends Application {
 
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
 			fxmlLoader.setController(this);
-			AnchorPane root = (AnchorPane) fxmlLoader.load();
+			root = (VBox) fxmlLoader.load();
 
 			fxmlLoader = new FXMLLoader(getClass().getResource("/window0.fxml"));
 			fxmlLoader.setController(this);
-			GridPane window = fxmlLoader.load();
+			windows.add((GridPane)fxmlLoader.load());
 
-			root.getChildren().add(window);
+			fxmlLoader = new FXMLLoader(getClass().getResource("/window1.fxml"));
+			fxmlLoader.setController(this);
+			windows.add((GridPane)fxmlLoader.load());
+
+			fxmlLoader = new FXMLLoader(getClass().getResource("/window2.fxml"));
+			fxmlLoader.setController(this);
+			windows.add((GridPane)fxmlLoader.load());			
+			
+			windowCurrent = windows.get(1);
+			root.getChildren().add(windowCurrent);
 
 			Scene scene = new Scene(root, 600, 400);
 			scene.getStylesheets().add(getClass().getResource("/main.css").toExternalForm());
@@ -76,5 +116,42 @@ public class Main extends Application {
 			e.printStackTrace();
 
 		}
-	}	
+	}
+	
+	@FXML
+	public void setWindow0(Event event) {
+
+		setWindow(event, 0);
+		
+	}
+
+	@FXML
+	public void setWindow1(Event event) {
+
+		setWindow(event, 1);
+		
+	}
+	
+	@FXML
+	public void setWindow2(Event event) {
+
+		setWindow(event, 2);
+		
+	}
+	
+	
+	private void setWindow(Event event, int idx_new) {
+
+		int idx_current = windows.indexOf(windowCurrent);
+
+		if (idx_current == idx_new) {
+			return;
+		}
+		
+		root.getChildren().remove(windowCurrent);
+		windowCurrent = windows.get(idx_new);
+		root.getChildren().add(windowCurrent);
+		
+	}
+	
 }
